b62afc912f27ed7b779b32a1d49dab5382138e18
namtaehyun <taehyunnam8034@gmail.com>
2021-11-13 23:38:32 +0900

    Add Chat Message List

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,42 @@ import 'package:english_words/english_words.dart';
 
 void main() => runApp(ChatApp());
 
+class ChatMessage extends StatelessWidget {
+  ChatMessage({
+    required this.text,
+    Key? key,
+  }) : super(key: key);
+
+  final String text;
+  final String _name = 'Taehyun Nam';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            margin: const EdgeInsets.only(right: 16.0),
+            child: CircleAvatar(child: Text(_name[0])),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(_name, style: Theme.of(context).textTheme.headline4),
+              Container(
+                margin: const EdgeInsets.only(top: 5.0),
+                child: Text(text),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
 // Chat App
 
 class ChatApp extends StatelessWidget {
@@ -21,10 +57,17 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  final List<ChatMessage> _messages = [];
   final _textController = TextEditingController();
+  final FocusNode _focusNode = FocusNode();
 
   void _handleSubmitted(String text) {
     _textController.clear();
+    var message = ChatMessage(text: text);
+    setState(() {
+      _messages.insert(0, message);
+    });
+    _focusNode.requestFocus();
   }
 
   Widget _buildTextComposer() {
@@ -38,13 +81,18 @@ class _ChatScreenState extends State<ChatScreen> {
                 onSubmitted: _handleSubmitted,
                 decoration:
                     const InputDecoration.collapsed(hintText: 'Send a message'),
+                focusNode: _focusNode,
               ),
             ),
-            Container(
-              margin: const EdgeInsets.symmetric(horizontal: 4.0),
-              child: IconButton(
-                  icon: const Icon(Icons.send),
-                  onPressed: () => _handleSubmitted(_textController.text)),
+            IconTheme(
+              data:
+                  IconThemeData(color: Theme.of(context).colorScheme.secondary),
+              child: Container(
+                margin: const EdgeInsets.symmetric(horizontal: 4.0),
+                child: IconButton(
+                    icon: const Icon(Icons.send),
+                    onPressed: () => _handleSubmitted(_textController.text)),
+              ),
             )
           ],
         ));
@@ -54,7 +102,19 @@ class _ChatScreenState extends State<ChatScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('FriendlyChat')),
-      body: _buildTextComposer(),
+      body: Column(children: [
+        Flexible(
+            child: ListView.builder(
+          padding: const EdgeInsets.all(8.0),
+          reverse: true,
+          itemBuilder: (_, index) => _messages[index],
+          itemCount: _messages.length,
+        )),
+        const Divider(height: 1.0),
+        Container(
+            decoration: BoxDecoration(color: Theme.of(context).cardColor),
+            child: _buildTextComposer()),
+      ]),
     );
   }
 }
